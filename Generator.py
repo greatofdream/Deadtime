51012e28da7cf56609e8498da6c07c7a57c0be1a
greatofdream <greatofdream@gmail.com>
2024-12-06 14:54:13 +0000

    fix the bonsai generator

--- Generator.py
+++ Generator.py
@@ -23,14 +23,14 @@ class Bonsai():
         self.TBIN = self.bonsaiLikelihoodReader.TBIN
         self.R_js = bonsaiLikelihood[::-1]
         self.bonsaiIntegration = np.insert(np.cumsum((self.R_js[:-1] + self.R_js[1:])/2), 0, 0) * self.bonsaiLikelihoodReader.TBIN
-        self.t_l, self.t_r = (-bonsaiLikelihood.shape[0] + self.bonsaiLikelihoodReader.offsets[1]) * self.bonsaiLikelihoodReader.TBIN, (self.bonsaiLikelihoodReader.offsets[1]-1) * self.bonsaiLikelihoodReader.TBIN
+        self.t_l, self.t_r = (-bonsaiLikelihood.shape[0] + int(self.bonsaiLikelihoodReader.offsets[1])) * self.bonsaiLikelihoodReader.TBIN, int(self.bonsaiLikelihoodReader.offsets[1]-1) * self.bonsaiLikelihoodReader.TBIN
 
         self.bonsaiLikelihood_fun = interp1d((np.arange(-bonsaiLikelihood.shape[0], 0) + self.bonsaiLikelihoodReader.offsets[1]) * self.bonsaiLikelihoodReader.TBIN, bonsaiLikelihood[::-1], bounds_error=False, fill_value=0)
         self.bonsaiIntegration_fun = interp1d((np.arange(-bonsaiLikelihood.shape[0], 0) + self.bonsaiLikelihoodReader.offsets[1]) * self.bonsaiLikelihoodReader.TBIN, self.bonsaiIntegration, bounds_error=False, fill_value=(0, 1))
         self.bonsaiIntegration_reverse_fun = interp1d(self.bonsaiIntegration, (np.arange(-bonsaiLikelihood.shape[0], 0) + self.bonsaiLikelihoodReader.offsets[1]) * self.bonsaiLikelihoodReader.TBIN, bounds_error=False, fill_value=(100, 100))
         self.fun = self.bonsaiLikelihood_fun
         self.integration_fun = self.bonsaiIntegration_fun
-        self.interation_reverse_fun = self.bonsaiIntegration_reverse_fun
+        self.integration_reverse_fun = self.bonsaiIntegration_reverse_fun
 
 class Generator():
     def __init__(self, model='rectangle'):
